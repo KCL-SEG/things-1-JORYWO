1c9b960d49c45df02c24288d736edbbccdda8f10
Joshua Wong <jorywo@gmail.com>
2022-10-09 18:12:42 +0100

    Add basic things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Thing(AbstractUser):
+    name = models.CharField()
+    description = models.CharField()
+    quantity = models.IntegerField()
